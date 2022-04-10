aad2d97ed79d8e6a70a2d6fae284497091d14b39
EC2 Default User <ec2-user@ip-172-31-46-206.ec2.internal>
2022-04-10 09:32:55 +0000

    [Add]show,index,editページの作成,routing設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,28 @@
 class BooksController < ApplicationController
 
   def index #一覧、投稿画面が同じ
-    @book = Book.all
+    @books = Book.all
     @book = Book.new
   end
 
   def create #保存機能
     book = Book.new(book_params)
     book.save
-    redirect_to "/"
+    redirect_to "/books/#{book.id}"
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to "/books/#{book.id}"
   end
 
   private
